8da1e507fb1bcea7e8750a6e21582d4198734e5b
Rojsun Parameshwaran <84145744+rojsun25@users.noreply.github.com>
2022-06-21 15:50:16 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from file_checker.extract_and_parse_file_from_zip import extract_and_parse_file_from_zip
 
-# inputfile - File directory or the ZipFile
-# destination - Extraction directory
-# extract - selected path within the directory 
-
-inputfile = "10th to 16th april 2022.zip"       
+inputfile = "10th to 16th april 2022.zip"
 destination = "unzipped"                        
 extract = "unzipped/10th to 16th april 2022/"   
 

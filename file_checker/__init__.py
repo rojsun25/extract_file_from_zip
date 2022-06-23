ebc5909240db840aa33b8f36b034134abe71d460
Rojsun Parameshwaran <84145744+rojsun25@users.noreply.github.com>
2022-06-23 11:15:15 +0100

    Delete __init__.py

--- file_checker/__init__.py
+++
@@ -1,7 +0,0 @@
-import extract_and_parse_file_from_zip as ex
-
-inputfile = "10th to 16th april 2022.zip"
-destination = "unzipped"                        
-extract = "unzipped/10th to 16th april 2022/" 
-
-ex.extract_and_parse_file_from_zip(inputfile,destination,extract)
